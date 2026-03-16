d7af6001ae012f3d82ee6a603e332973134e054d
Arif Mohamed Khan Rabi Ahamad <arabiaha@syr.edu>
2026-03-16 06:15:33 +0000

    Final

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,8 @@
-
--- Use the `ref` function to select from other models
+{{ config(
+    database='ARIF_DB',
+    schema='DBT_ARABIAHAMAD',
+    materialized='table'
+) }}
 
 select *
 from {{ ref('my_first_dbt_model') }}
-where id = 1
